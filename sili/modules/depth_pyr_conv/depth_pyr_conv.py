Initial work on fixing conv backwards. For some reason full backprop op has waaaaaaaay too many outputs.

sili/modules/depth_pyr_conv/depth_pyr_conv.py
@@ -33,6 +33,16 @@ def one_hot_mat(n, x, y):
     A[x,y] = 1
     return A
 
+def calc_pyr_reduce_buf_size(levels, workgroup_size, channels = 3):
+    num_levels = levels[1]
+    levels_array = levels[2:]
+    total_size = 0
+    for i in range(num_levels):
+        width = levels_array[3*i+1]
+        height = levels_array[3*i+2]
+        total_size += int(np.ceil(width*height*channels/workgroup_size))
+    return total_size
+
 class DepthPyrConv(object):
     def __init__(self,
                  gpu: GPUManager,
@@ -108,34 +118,33 @@ class DepthPyrConv(object):
         if backprop_conv:
             self.has_backward = True
             self.backprop_conv = True
-            shad_conv_back_prepool_1 = get_shader(file_path + os.sep + 'depth_pyr_backward_conv_prepool.comp')
+            shad_conv_back_prepool_1 = get_shader(file_path + os.sep + 'depth_conv_back_prepool_full.comp')
             shad_conv_back_prepool_2 = get_shader(file_path + os.sep + 'index2_value2_max_reduction.comp')
             shad_conv_back = get_shader(file_path + os.sep + 'depth_pyr_backward_conv.comp')
             shad_conv_reduce = get_shader(file_path + os.sep + 'conv_err_sum_reduce.comp')
 
             # private buffers
-            buf_conv_prepool = self.gpu.manager.tensor(np.zeros([int(np.ceil(self.in_img_pyr.size * 4 / self.gpu.max_workgroup_invocations))]))
-
+            self.buf_conv_prepool = self.gpu.manager.tensor(np.zeros([int(4*self.in_img_pyr.levels[1] *np.ceil(calc_pyr_reduce_buf_size(self.in_img_pyr.levels, self.gpu.max_workgroup_invocations)))]))
 
             # no special backwards input/output buffers, because the backprop ends in the conv buffer inside this object
             self.algorithm_conv_prepool_1 = self.gpu.manager.algorithm(
-                [self.in_img_pyr.image_buffer, self.out_pyr_err.image_buffer, buf_conv_prepool, self.in_img_pyr.pyr_lvl_buffer, self.depth_conv.buffer,
+                [self.in_img_pyr.image_buffer, self.out_pyr_err.image_buffer, self.buf_conv_prepool, self.in_img_pyr.pyr_lvl_buffer, self.depth_conv.buffer,
                  self.depth_conv_str.buffer],
                 spirv=shad_conv_back_prepool_1,
-                workgroup=[int(np.ceil(self.in_img_pyr.size * self.in_img_pyr.levels[1] / self.gpu.max_workgroup_invocations)), 0, 0],
+                workgroup=[int(np.ceil((self.buf_conv_prepool.size()/4) * self.in_img_pyr.levels[1] / self.gpu.max_workgroup_invocations)), 0, 0],
                 # workgroup=[1, 0, 0],
                 spec_consts=np.asarray([self.gpu.max_workgroup_invocations], dtype=np.uint32).view(np.float32)
             )
 
             self.conv_prepool_reductions = []
-            last_s0 = prev_s0 = int(buf_conv_prepool.size() / 4)
-            s0 = int(np.ceil(buf_conv_prepool.size() / 4 / self.gpu.max_workgroup_invocations))
+            last_s0 = prev_s0 = int(self.buf_conv_prepool.size() / 4)
+            s0 = int(np.ceil(self.buf_conv_prepool.size() / 4 / self.gpu.max_workgroup_invocations))
             while True:
                 if s0 <= 1:  # do while loop to ensure we include the final reduce
                     break
                 self.conv_prepool_reductions.append(
                     self.gpu.manager.algorithm(
-                        [buf_conv_prepool, self.depth_conv_str.buffer],
+                        [self.buf_conv_prepool, self.depth_conv_str.buffer],
                         spirv=shad_conv_back_prepool_2,
                         workgroup=[last_s0, 0, 0],
                         spec_consts=np.asarray([s0, prev_s0, self.in_img_pyr.levels[1]], dtype=np.uint32).view(np.float32)
@@ -143,8 +152,8 @@ class DepthPyrConv(object):
                 )
                 last_s0 = int(np.ceil(prev_s0 / s0))
                 prev_s0 = s0
-                s0 = int(np.ceil(s0 / 2))
-
+                s0 = int(np.ceil(s0 / last_s0))
+            last_s0 = prev_s0
             memory_sensitive_divisor = int(
                 2 ** int(np.log2(self.gpu.maxComputeSharedMemorySize / (
                             self.in_img_pyr.levels[1] * self.in_img_pyr.levels[1] * 4 * 2))))
@@ -155,7 +164,7 @@ class DepthPyrConv(object):
                 (int(np.ceil(last_s0/memory_sensitive_divisor)), self.in_img_pyr.levels[1], self.in_img_pyr.levels[1])))
 
             self.algorithm_conv_back = self.gpu.manager.algorithm(
-                [buf_conv_prepool, self.depth_conv_err.buffer, self.depth_conv_contrib.buffer],
+                [self.buf_conv_prepool, self.depth_conv_err.buffer, self.depth_conv_contrib.buffer],
                 spirv=shad_conv_back,
                 workgroup=[int(np.ceil(last_s0 / memory_sensitive_divisor)), 0, 0],
                 spec_consts=np.asarray([memory_sensitive_divisor, self.in_img_pyr.levels[1], last_s0], dtype=np.uint32).view(

sili/modules/mse_loss/mse_loss.py
@@ -42,13 +42,13 @@ class MSELoss(object):
         if not isinstance(error_buffer, kp.Tensor):
             raise TypeError("Error Buffer must be a buffer attached to a device")
         else:
-            self.err_buff = error_buffer
+            self.err_grad_buff = error_buffer
 
         self.has_forward = forward
         if forward:
             self.forward_shader = get_shader(file_path + os.sep + 'mse_loss.comp')
 
-            #self.err_buff = gpu.buffer(np.zeros_like(self.act_buff.data()))
+            self.err_buff = gpu.buffer(np.zeros_like(self.act_buff.data()))
 
             # PIPELINE OBJECTS:
             # FORWARD
@@ -71,8 +71,6 @@ class MSELoss(object):
         self.has_backward = backprop
         if backprop:
             self.backward_shader = get_shader(file_path + os.sep + 'mse_loss_backward.comp')
-
-            self.err_grad_buff = gpu.buffer(np.zeros_like(self.act_buff.data()))
 
             self.backward_input_buffers = [self.pred_buff, self.act_buff]
             self.backward_output_buffers = [self.err_grad_buff]
@@ -143,7 +141,7 @@ class MSELoss(object):
             self.basic_sequence = self.gpu.manager.sequence()
             self.basic_sequence.record(kp.OpTensorSyncDevice([*self.backward_input_buffers]))
             for b in self.backward_ops():
-                self.basic_sequence.record(kp.OpAlgoDispatch(b))
+                self.basic_sequence.record(b)
             self.basic_sequence.record(kp.OpTensorSyncLocal([*self.backward_output_buffers]))
         self.basic_sequence.eval()
         return self.err_grad_buff
@@ -170,7 +168,8 @@ if __name__ == '__main__':
     gpu = GPUManager()
     act_buf = ImageBuffer(gpu, im)
     pred_buf = ImageBuffer(gpu, im+np.random.normal(0.1, 0.5, im.shape))
-    err = MSELoss(gpu, pred_buf.buffer, act_buf.buffer, forward=False, backprop=True)
+    err_buf = ImageBuffer(gpu, np.zeros_like(im))
+    err = MSELoss(gpu, pred_buf.buffer, act_buf.buffer, err_buf.buffer, forward=False, backprop=True)
     err.display_basic_backward_sequence(shape=im.shape)
 
     # im_pyr = pyr.run_basic_forward_sequence(im)

test/simple_learning_sequence.py
@@ -22,7 +22,7 @@ class GPUMLPipeline(object):
         for m in self.module_list:
             if m.has_forward and m.forward_input_buffers:
                 self.upload_sequence.record(kp.OpTensorSyncDevice([*m.forward_input_buffers]))
-        for m in self.module_list:
+        for m in reversed(self.module_list):
             if m.has_backward and m.backward_input_buffers:
                 self.upload_sequence.record(kp.OpTensorSyncDevice([*m.backward_input_buffers]))
         for m in self.module_list:
@@ -35,7 +35,7 @@ class GPUMLPipeline(object):
         for m in self.module_list:
             for f in m.forward_ops():
                 self.full_sequence.record(f)
-        for m in self.module_list:
+        for m in reversed(self.module_list):
             for b in m.backward_ops():
                 self.full_sequence.record(b)
         for m in self.module_list:
@@ -87,13 +87,14 @@ if __name__ == "__main__":
         # set these so that only these are moved to/from the gpu
         # can be set to error or other info for debugging
         input_buffers=im_pyr.forward_input_buffers,
-        output_buffers=depth_pyr_conv_optim.optim_output_buffers
+#        output_buffers=depth_pyr_conv.forward_output_buffers
+        output_buffers=depth_pyr_conv_optim.optim_output_buffers + [depth_pyr_conv.buf_conv_prepool] + [depth_pyr_conv.out_pyr_err.image_buffer]
     )
     d = DirectDisplay()
     while not d.window.is_closing:
         out_buf = runner.run_once(im)
         for i,o in enumerate(depth_pyr_conv_optim.optim_output_buffers):
-            o_np = o.data().reshape(17,17,1)
+            o_np = o.data()[:17*17].reshape(17,17,1)
             o_np = o_np.repeat(3, axis=-1)
             min_o = np.min(o_np)
             max_o = np.max(o_np)
@@ -103,4 +104,33 @@ if __name__ == "__main__":
                 num[:]= 0.5
                 den = 1.0
             d.imshow(f"im {i}", num/den)
+        for i, o in enumerate(depth_pyr_conv.out_pyr_err.get()):
+            min_o = np.min(o)
+            max_o = np.max(o)
+            num = (o - min_o)
+            den = (max_o - min_o)
+            if den == 0.0:
+                num[:] = 0.5
+                den = 1.0
+            d.imshow(f"im err {i}", num / den)
+        pre = depth_pyr_conv.buf_conv_prepool.data().reshape(-1,4, 1)
+        pre = pre.repeat(3, axis=-1)
+        min_o = np.min(pre)
+        max_o = np.max(pre)
+        num = (pre - min_o)
+        den = (max_o - min_o)
+        d.imshow(f"im pre", num/den)
         d.update()
+    '''while not d.window.is_closing:
+        out_buf = runner.run_once(im)
+
+        for i, o in enumerate(depth_pyr_conv.out_pyr.get()):
+            min_o = np.min(o)
+            max_o = np.max(o)
+            num = (o - min_o)
+            den = (max_o - min_o)
+            if den == 0.0:
+                num[:] = 0.5
+                den = 1.0
+            d.imshow(f"im {i}", num / den)
+        d.update()'''